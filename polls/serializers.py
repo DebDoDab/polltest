18af8c2d20211487a51d13e6efe7c6aad2afd628
DebDoDab <debbius3@gmail.com>
2020-05-13 15:25:36 +0300

    Fix polls

--- polls/serializers.py
+++ polls/serializers.py
@@ -4,7 +4,7 @@ from .models import Poll
 
 class PollSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=256)
-    startDate = serializers.DateTimeField()
+    startDate = serializers.DateTimeField(read_only=True)
     endDate = serializers.DateTimeField()
     description = serializers.CharField(max_length=512)
 
--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Poll
 from .serializers import PollSerializer
+from django.utils import timezone
 
 
 # Create your views here.
 class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+
+    def get_queryset(self):
+        queryset = Poll.objects.all()
+        active = self.request.query_params.get('active', None)
+        if active is not None:
+            queryset = queryset.filter(endDate__gt=timezone.now())
+        return queryset
